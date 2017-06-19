39cddf610f4669c650a7d38069667068033f7bc8
Daniel Neel <dneel004@fiu.edu>
2017-06-19 13:33:41 -0400

    ADD working test wakeword

--- src/js_test/test_wakeword.js
+++ src/js_test/test_wakeword.js
@@ -38,8 +38,8 @@ function startWakeUpRecognition(){
   console.log('<== config wakeword recognition..')
   var wakeword_config = new matrixMalosBuilder.WakeWordParams;
   wakeword_config.set_wake_word("MARRIOTT");
-  wakeword_config.set_lm_path("../../assets/commands.lm");
-  wakeword_config.set_dic_path("../../assets/commands.dic");
+  wakeword_config.set_lm_path("/home/pi/Marriott-Demo/assets/7719.lm");
+  wakeword_config.set_dic_path("/home/pi/Marriott-Demo/assets/7719.dic");
   wakeword_config.set_channel(matrixMalosBuilder.WakeWordParams.MicChannel.channel8);
   wakeword_config.set_enable_verbose(false)
   sendConfigProto(wakeword_config);
